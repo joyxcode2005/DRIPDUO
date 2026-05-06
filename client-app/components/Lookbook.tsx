2ddc93ba0fb2a166a477aa8768e5c51e96856cde
joy <133984488+joyxcode2005@users.noreply.github.com>
2026-05-07 00:36:30 +0530

    fixed lookbook issue

--- client-app/components/Lookbook.tsx
+++ client-app/components/Lookbook.tsx
@@ -1,20 +1,34 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import Reveal from './Reveal';
 
-export default function Lookbook({ product }: { product: any }) {
+interface LookbookProps {
+    id: string;
+    name: string;
+    product_images: {
+        url: string;
+        is_primary: boolean;
+    }[];
+}
 
-    console.log("Rendering Lookbook for product:", product);
-    if (!product.product_images || product.product_images.length === 0) return null;
+export default function Lookbook({ product }: { product: LookbookProps }) {
+    if (!product.product_images?.length) return null;
+
     return (
         <>
-            {[...product.product_images].map((Image: any, i: number) => (
+            {product.product_images.map((img, i) => (
                 <Reveal key={i} className="shrink-0" threshold={0.12}>
-                    <div className="relative aspect-2/3 w-[75vw] sm:w-[45vw] md:w-[30vw] lg:w-[22vw] border-r border-y border-(--gray-800) overflow-hidden bg-(--gray-900) first:border-l">
-                    <Link href={`/products/${product.id}`} className="absolute inset-0 z-20" />
+                    <div className="relative aspect-[2/3] w-[75vw] sm:w-[45vw] md:w-[30vw] lg:w-[22vw] border-r border-y border-(--gray-800) overflow-hidden bg-(--gray-900) first:border-l">
+                        <Link
+                            href={`/products/${product.id}`}
+                            className="absolute inset-0 z-20"
+                        />
+
                         <Image
-                            src={Image.url}
-                            alt={`Look ${(i % product.product_images.length) + 1}`}
-                            className="h-full w-full object-cover transition-transform duration-[2s] hover:scale-105"
+                            src={img.url}
+                            alt={`Look ${i + 1}`}
+                            fill
+                            className="object-cover transition-transform duration-[2s] hover:scale-105"
                         />
                     </div>
                 </Reveal>
